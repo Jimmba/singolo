3bf445d7d5cffd85fc3b2c9211068e24bd2357d3
Jimmba <gimvit@gmail.com>
2020-03-13 18:51:36 +0300

    fix: add anchors, add scss file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const MENU = document.getElementById("menu");
 
 MENU.addEventListener('click', (event) =>
 {
-    MENU.querySelectorAll('li').forEach(el => {
+    MENU.querySelectorAll('a').forEach(el => {
         el.classList.remove('active');
     });
     event.target.classList.add('active');
